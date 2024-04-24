d3dd9dcd8d3932471a9447440582f6dc5a947a7b
MPal <118168603+Manu-bit183@users.noreply.github.com>
2024-04-24 07:23:03 +0200

    Messages made

--- src/App.js
+++ src/App.js
@@ -1,14 +1,30 @@
-import React from 'react';
-import './App.css';
+import React, {useState} from 'react';
 import header from './components/header';
 import figure from './components/figure';
+import Word from './components/Word';
+import wrongLetters from './components/wrongLetters';
+
+import './App.css';
+
+const figureParts = document.querySelectorAll('.figure-part');
+
+const words = ['application', 'programming', 'interface', 'wizard'];
+
+let selectedWord = words[Math.floor(Math.random() * words.length)];
+
 
 function App() {
+  const [playable, setPlayable] = useState(true);
+  const [correctLetters, setcorrectLetters] = useState([])
+  const [wrongLetters, setwrongLetters] = useState([])
+
   return (
     <>
       <header />
       <div className='game-container'>
-        <figure></figure>
+        <figure />
+        <wrongLetters />
+        <word />
       </div>
     </>
   );
---
+++ src/components/wrongLetters.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const wrongLetters = () => {
+  return (
+    <div>
+      <div class="wrong-letters-container">
+        <div id="wrong-letters"></div>
+      </div>
+
+      <div class="word" id="word"></div>
+    </div>
+  )
+}
+
+export default wrongLetters;
